f6f5704a7cb32d601f7070d8f0eed7f12423810b
Alban Bochsler <alban.bochsler@gmail.com>
2017-10-23 20:18:51 +0200

    Contactform changes / Minor bug fixes 2.2

--- app/src/form-process.php
+++ app/src/form-process.php
@@ -86,10 +86,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $subject = 'Anfrage über Kontaktformular';
         if (mail($to, $subject, $message_body)){
             $success = "Ihre Nachricht wurde übermittelt, Sie werden in kürze benachrichtigt!";
-            $error = "Ihre Nachricht wurde nicht übermittelt, bitte füllen Sie die nötigen Felder aus.";
+
             //reset form values
             $name = $email = $phone = $message = $url = $firstname = '';
             $gender = '';
+        } else {
+            $error = "Ihre Nachricht wurde nicht übermittelt, bitte füllen Sie die nötigen Felder aus.";
         }
 
     }
